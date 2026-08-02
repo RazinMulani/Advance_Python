22b2cb3d968eb67a71b434f691fc73eef356860e
Mohamad Rajin Rafik Mulani <159556126+RazinMulani@users.noreply.github.com>
2026-08-02 16:32:01 +0530

    Add files via upload

---
+++ Python Library/TenserFlow/shape.py
@@ -0,0 +1,7 @@
+# Tensor Shape
+# What is Shape in Tensor?
+# Tensor Shape Tells us The Structrue of a tensor, including: Number of dimension, number of rows, number
+# of columns, and size of each dimension
+# in simple words, tensor describe how data is aaranged in inside a Tensor.
+
+# Syntax: tensor.shape()
